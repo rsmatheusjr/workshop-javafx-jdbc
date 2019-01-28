2efd755636f43e6440af41f5d151ccefa841eb80
Roberto Matheus <rsmatheusjr@hotmail.com>
2019-01-28 19:56:39 -0200

    Padrao de projeto Observer para atualizar a TableView

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -23,6 +26,9 @@ public class DepartmentFormController implements Initializable{
 	private Department entity;
 	
 	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
 	
 	
 	@FXML
@@ -46,11 +52,20 @@ public class DepartmentFormController implements Initializable{
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangerListeners();
 			Utils.currentStage(event).close();
 			
 		}catch(DbException e) {
 			Alerts.showAlert("Error saving object.", null, e.getMessage(), AlertType.ERROR);
 		}
+	}
+	private void notifyDataChangerListeners() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+		
+		
+		
 	}
 	private Department getFormData() {
 		Department obj = new Department();
@@ -73,6 +88,11 @@ public class DepartmentFormController implements Initializable{
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {	
